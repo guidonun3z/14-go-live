6630b782f414b64169f4e54d4b262dda98073af0
Guido Nuñez Apaza <37349399+guidonun3z@users.noreply.github.com>
2022-04-08 17:54:28 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Anfibio',
+            'Fredy',
             {
                 font: font,
                 size: 0.5,
