68b4fd1cff7aa7a54440dbf95081fc33224b023e
Guido Nuñez Apaza <37349399+guidonun3z@users.noreply.github.com>
2023-01-31 20:33:41 -0500

    Update script.js

--- src/script.js
+++ src/script.js
@@ -37,7 +37,7 @@ fontLoader.load(
 
         // Text
         const textGeometry = new TextGeometry(
-            'Fredy',
+            'Pamela',
             {
                 font: font,
                 size: 0.5,
